b3bb3f1585848a47a4f602239719ad69d0a7460e
klfz <626952407@qq.com>
2019-09-21 17:44:38 +0800

    Updates

---
+++ precache-manifest.7029edec545e1fec9f0c790176ccb1a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede14cc89ea902ec395fcf364d30e463",
+    "url": "/wtg/index.html"
+  },
+  {
+    "revision": "56c189a19a4d4e17c1f3",
+    "url": "/wtg/static/css/main.1e339e60.chunk.css"
+  },
+  {
+    "revision": "6e808919b2b77cab34c0",
+    "url": "/wtg/static/js/2.f3f35e82.chunk.js"
+  },
+  {
+    "revision": "56c189a19a4d4e17c1f3",
+    "url": "/wtg/static/js/main.0240bdd0.chunk.js"
+  },
+  {
+    "revision": "6f7dbcaaa7043096220a",
+    "url": "/wtg/static/js/runtime~main.9b37c722.js"
+  }
+]);
